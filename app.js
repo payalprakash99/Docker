f5126d968668a332fb7feb3ee217714bcc8a4107
Payal P Shah <payalprakash.shah@gmail.com>
2021-03-09 11:15:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// app.js
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send(`Hello Docker!`));
+
+app.listen(3000, () => {
+  console.log(`Example app listening on port 3000!`);
+});
